a8144ab6a64b10e0dade9b49776c626008d7c49c
Ioannis Tsikelis <tsikelis.i@protonmail.com>
2022-06-28 18:01:20 +0300

    Code tested and ready.

--- src/Part-B/main.py
+++ src/Part-B/main.py
@@ -3,21 +3,172 @@ import numpy as np
 import pandas as pd
 import preprocess as prep
 
-POP_COUNT = 5
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+
+import tensorflow as tf
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import backend as K
+
+import os
+import pickle
+
+import matplotlib.pyplot as plt
+
+## Repeat algorithm 10 times in each case.
+GA_ITERATIONS = 4
+
+## Maximum generations allowed.
+MAX_ITER = 500
+## Score improvement threshold.
+THRESHOLD = 2e-05
+
+## Population size.
+POP_SIZE = 20
+## Crossover probability.
+CROSS_PROB = .6
+## Mutation probability.
+MUTATION_PROB = .1
 
 def main():
-    # Create a population.
-    pop = prep.create_population(POP_COUNT, 1.6/3)
 
     # Get the BoW model from the previous part.
-    X = prep.dat_to_bow_2(r'src/Part-B/data/dat/mini-test.dat')
-    
-    tf_matrix = prep.calc_tf(pop, X)
+    X = prep.dat_to_bow_2(r'src/Part-B/data/dat/train-data.dat')
+    tf_idf = prep.calc_tf_idf(prep.calc_tf(X), prep.calc_idf(X))
 
-    print("FINAL SUM")
-    print(tf_matrix.sum())
+    ## Create an array to store the best individual scores each time.
+    best_scores = np.zeros(1, dtype=float)
+    total_generations = np.empty(0, dtype=int)
+
+    for i in range(0, GA_ITERATIONS):
+
+        # Create a population.
+        pop = prep.create_population(POP_SIZE, 1.6/3)
+
+        ## Reset initial metric values.
+        best_score_prev = .00001
+        generation_count = 0
+        ## To record each generation's's scores.
+        generation_scores = np.empty(0, dtype=float)
+        while (generation_count<MAX_ITER):
+            ## Get scores for current population.
+            scores = prep.evaluate(pop, tf_idf)
+            ## Compare the current best individual's score with the previous one.
+            best_score_curr = np.max(scores)
+            score_increase = ((best_score_curr/best_score_prev)-1)
+            ## Make sure algorithm runs at least 30 times.
+            if((np.absolute(score_increase) < THRESHOLD) & (generation_count >= 30)):
+                break
+
+            ## Create new population
+            pop = prep.roulette(pop, scores, POP_SIZE)
+            pop = prep.cross(pop, CROSS_PROB)
+            pop = prep.mutate(pop, MUTATION_PROB)
+
+            ## Set current mean score as previous and loop.
+            best_score_prev = best_score_curr
+            generation_count += 1 
+
+            ## Record each generation's best score.
+            generation_scores = np.append(generation_scores, best_score_curr)
+            print('Generation:', generation_count)
+            print('Score:', best_score_curr)
+
+        ## If current iteration had better mean score, update best scores
+        if (np.mean(generation_scores) > np.mean(best_scores)):
+            print('Updated')
+            best_scores = generation_scores
+
+        ## Record generation count.
+        total_generations = np.append(total_generations, generation_count)
+
+    ## Create plots and show mean data.
+    plt.figure()
+    plt.xlabel('Generations')
+    plt.ylabel('Mean Score')
+    plt.plot(best_scores, label='Score/Generation')
+    plt.legend(loc='best')
+    print('Mean generation count: ', np.mean(total_generations))
+    print('Mean best score: ', np.mean(best_scores))
+
+    plt.show()
+
+############  NN  ############
+
+
+# print('Loading Data.')
+# ## Get BoW dataframe for train dataset.
+# X = prep.dat_to_bow_std(r'src/Part-B/data/dat/train-data.dat')
+# ## Get BoW dataframe for test dataset
+# X_eval = prep.dat_to_bow_std(r'src/Part-B/data/dat/test-data.dat')
+# # Load the test labels.
+# Y = np.genfromtxt(r'src/Part-B/data/dat/train-label.dat', delimiter=' ',
+#                   dtype='int')
+# Y_eval = np.genfromtxt(r'src/Part-B/data/dat/test-label.dat', delimiter=' ',
+#                        dtype='int')
+
+# print('Labels Loaded!')
+
+# ## Create a callback for early stopping
+# callback = tf.keras.callbacks.EarlyStopping(
+#     monitor='accuracy', min_delta=0.0003, patience=3)
+
+# ## Split Data To Training And Testing Data 5-Fold
+# kfold = KFold(n_splits=2, shuffle=True)
+# prev_loss = 100
+# for curr_fold, (train, test) in enumerate(kfold.split(X)):
+
+#     ## Specify Model's Input.
+#     inputs = keras.Input(shape=(X.shape[1], ))
+#     ## Create One Hidden Layer.
+#     x = keras.layers.Dense(units=4270, activation='relu',
+#                            activity_regularizer=keras.regularizers.L2(0.9))(inputs)
+#     ## Add second Hidden Layer
+#     x = keras.layers.Dense(units=8540, activation='relu',
+#                            activity_regularizer=keras.regularizers.L2(0.9))(x)
+#     ## Specify Model's Output.
+#     outputs = keras.layers.Dense(units=20, activation='sigmoid',
+#                                  activity_regularizer=keras.regularizers.L2(0.9))(x)
+#     ## Create Model.
+#     model = keras.Model(inputs=inputs, outputs=outputs)
+
+#     ## Compile Model.
+#     model.compile(loss=keras.losses.BinaryCrossentropy(),
+#                   optimizer=tf.keras.optimizers.SGD(
+#         learning_rate=0.001, momentum=0.2),
+#         metrics=['mse', 'accuracy'])
+#     ## Print Model's Summary.
+#     model.summary()
+#     ## Fit Model and store each epoch's metrics in history.
+#     history = model.fit(X, Y, epochs=150,
+#                         verbose=1,
+#                         callbacks=[callback]
+#                         )
+
+#     ## Evaluate the model and store results to file.
+#     eval = model.evaluate(X_eval, Y_eval, verbose=1)
+
+#     # Save history, eavluation results and model
+#     if(eval[0] < prev_loss):
+#         ## Store History.
+#         history_path = os.path.join(
+#             'src', 'Part-B', 'saves', 'history', 'history.pkl')
+#         f = open(history_path, 'wb')
+#         pickle.dump(history.history, f)
+#         f.close()
+#         ## Store Evaluation Results.
+#         eval_path = os.path.join(
+#             'src', 'Part-B', 'saves', 'evaluation', 'eval.pkl')
+#         f = open(eval_path, 'wb')
+#         pickle.dump(eval, f)
+#         f.close()
+#         # End If.
+#     # End For.
 
 
 if __name__ == '__main__':
-    for i in range(0,5):
-        main()
+    main()
+
--- src/Part-B/preprocess.py
+++ src/Part-B/preprocess.py
@@ -1,4 +1,6 @@
+from cmath import inf
 from operator import delitem
+from cv2 import mean
 import numpy as np
 import pandas as pd
 
@@ -6,12 +8,17 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import StandardScaler
 
 VOC_SIZE = 8520
-DOC_COUNT = 3
+DOC_COUNT = 8250
+
+SCALAR = 1000
+
 
 ## @package prep
 #  
-#  Some utility scripts for pre-processing the data.
+#  Some utility scripts for pre-processing the data. Enriched with functions 
+#  used by the Genetic Algorithm in Part B.
 #
+
 
 ## @ brief Create a vocabulary(dict) of type str(number): int(number).
 #
@@ -22,6 +29,35 @@ def get_voc():
 	strings = [str(x) for x in ints]
 	voc = dict(zip(strings, ints))
 	return voc
+
+
+## Function that creates a standardised BoW.
+#
+#  @param path The path to the .dat file.
+#  @param voc The vocabulary to be used by the CountVectorizer
+#
+#  @return bow The standardised BoW returned as a numpy ndarray.
+#
+def dat_to_bow_std(path):
+
+	## Create a vocabulary.
+	voc = get_voc()
+	
+	## Read dat file to a pandas dataframe.
+	df = pd.read_csv(path, header=None, names=['text'])
+	## Remove all sentence and word counters using regex.
+	df = df.replace(to_replace='<[0-9]*> ', value='', regex=True)
+	## Create a Vectorizer object with given vocabulary.
+	cv = CountVectorizer(vocabulary=voc)
+	# Set fixed_vocabulary boolean to True.
+	cv.fixed_vocabulary_ = True
+	## Create a sparse matrix where:
+	# - columns: Every word found in data.
+	# - rows: one for each document (line) in .dat file.
+	doc_vec = cv.fit_transform(df['text'])
+	## Return the standardized matrix.
+	bow = StandardScaler().fit_transform(doc_vec.toarray())
+	return bow
 
 ## Function that creates a standardised BoW.
 #
@@ -47,6 +83,7 @@ def dat_to_bow_2(path):
 	doc_vec = cv.fit_transform(df['text'])
 	return doc_vec.toarray()
 
+
 ## @brief Function that pads the .dat file contents with 0s at the end.
 #
 #  @param path The path to the .dat file.
@@ -66,6 +103,7 @@ def dat_to_pad(path):
 	arr = df.to_numpy(dtype='int')
 	return arr
 
+
 ## @brief Function that creates a population of 8520 by 1 vectors.
 #
 #  @param count The population count.
@@ -77,24 +115,23 @@ def create_population(count, prob):
 
 	## Create an empty population.
 	pop = np.empty((0, VOC_SIZE), dtype=int)
-	print(pop)
 	## Create 'count' individuals and add them to the population.
 	for i in range (0, count):
 		# Repeat until created individual has more than 1000 ones.
 		while True:
 			individual = np.random.choice([0, 1], size=(1, VOC_SIZE), 
 											p=[1-prob, prob])
-			print(individual)
 			## If individual has at least 1000 ones, break 
 			# and add it to the population.						
 			if np.sum(individual) >= 1000:
 				break
 
 		# Add new individual to population.
-		pop = np.append(pop, individual, axis=0)
+		pop = np.vstack([pop, individual])
 
 	## Return population.
 	return pop
+
 
 ## @brief Function that calculates the term frequency of an individual.
 #
@@ -102,35 +139,128 @@ def create_population(count, prob):
 #
 #  @param prob The probability to pick a word for the current individual.
 #
-#  @return pop A 8520 by count matrix of the population.
-def calc_tf(pop, bow):
+#  @return bow_tf A DOC_COUNT by WORD_COUNT ndarray with the tf values 
+#  of each word.
+def calc_tf(bow):
 
-	## Get row count of pop.
-	rows, _ = pop.shape
-	## Create a matrix to store the cumulative tf values of the population.
-	pop_tf = np.empty((0 , VOC_SIZE))
-	## For each individual in population (every row).
-	for i in range(0, rows):
-		## Create a vector to store the cumulative tf values of each word.
-		doc_tf = np.zeros((1, VOC_SIZE))
-		## For each document in vector.
-		for j in range(0, DOC_COUNT):
-			## Get frequency vector of each word selected in 
-			# the current individual.
-			freq = np.multiply(pop[i], bow[j])
-			## Add the accumulated term frequency to the vector.
-			doc_tf = doc_tf + freq
+	## Add bow row-wise to calculate total words in each document.
+	total_words_row_wise = np.sum(bow, axis=1)
 
-		## Calculate tf.
-		doc_tf = doc_tf/np.count_nonzero(bow[j])
-		print(doc_tf.max())
-		## Add the calculated tf for the current word in 
-		# the tf matrix for the population.
-		pop_tf = np.append(pop_tf, doc_tf, axis=0)
+	## Create a copy of the BoW with dtype=float.
+	tf_values = bow.astype(dtype=float)
+
+	## Divide each row with the corresponding word count to get the tf matrix.
+	for i in range(0, DOC_COUNT):
+		tf_values[i] = tf_values[i] / total_words_row_wise[i]
+
+	## Sum all tf values of each word and divide bu DOC_COUNT
+	mean_tf_values = tf_values.sum(axis=0)
+	mean_tf_values = np.divide(mean_tf_values, DOC_COUNT)
+	## Return the mean tf value for each word found in all documents.
+	return mean_tf_values
+
+
+## @brief Function that calculates the inverse document frequency of each word.
+#
+#  @param bow The BoW representation of the test data.
+#
+#  @return idf A (1, VOC_SIZE) array with the idf of each word.
+def calc_idf(bow):
+	## Replace all non-zero elements of bow with 1.
+	bow[bow>0] = 1
+	## Sum all ones to get each word's appearence in every document.
+	word_appearence = np.sum(bow, axis=0, dtype=int)
+	## Create an array of floats to store the calculated idf values.
+	idf_values = word_appearence.astype(dtype=float)
+	## Calculate the element-wise inverse of the array.
+	idf_values = np.reciprocal(idf_values)
+	## Infs occur when dividing by zero, so we replace them with 0 again.
+	idf_values[idf_values==inf] = 0
+	## Multiply with DOC_COUNT.
+	idf_values = np.multiply(idf_values, DOC_COUNT)
+	## Get element-wise lof of array.
+	idf_values = np.log10(idf_values)
+	## And again, remove infs.
+	## Infs occur when dividing by zero, so we replace them with 0 again.
+	idf_values[idf_values == -inf] = 0
+	## Return idf_values
+	return idf_values
+
+def calc_tf_idf(mean_tf_array, idf_array):
+	## Multiply each row of the matrix with the idf values for each word.
+	tf_idf = np.multiply(mean_tf_array, idf_array)
 	
-	## Return the tf matrix.
-	return pop_tf
+	return tf_idf
 
-def calc_idf():
-	pass
+def evaluate(pop, tf_idf):
+	## Create a matrix with only the selected words for each individual.
+	selected = np.multiply(pop, tf_idf)
 
+	## Divide with word count of each individual to get the final score.
+	score = np.sum(selected, axis=1)
+	score = np.divide(score, np.count_nonzero(selected, axis=1)-1000)*SCALAR
+	return score
+
+def roulette(pop, scores, pop_size):
+
+	## Calculate total score.
+	total_score = np.sum(scores)
+	## Calculate selection probabilities.
+	probs = np.divide(scores, total_score)
+
+	number_of_rows = pop.shape[0]
+	random_indices = np.random.choice(number_of_rows, size=pop_size, p=probs)
+	
+	new_pop = pop[random_indices, :]
+
+	return new_pop
+
+def cross(pop, cross_probability):
+	## Initialize an array with the selected individuals.
+	cross_pop = np.empty((0, 8520), dtype=int)
+	## Initialize an array to store the indices of the selected individuals.
+	cross_indices = np.empty(0, dtype=int)
+
+	## Select the individuals to be crossed from the population matrix.
+	for i in range(0, pop.shape[0]):
+
+		## Calculate a random number between 0 and 1.
+		rand = np.random.uniform()
+		## If number is less than the cross probability, select
+		#  individual for crossing.
+		if(rand < cross_probability):
+			## Append 
+			cross_pop = np.vstack([cross_pop, pop[i]])
+			## Also store index of selected individual.
+			cross_indices = np.append(cross_indices, i)
+
+	## Make sure that only an even number of individuals is selected.
+	if (np.mod(cross_pop.shape[0], 2)!=0):
+		## Select and remove a random individual from the cross set.
+		index_to_remove = np.random.randint(low=0, high=cross_pop.shape[0])
+		cross_pop = np.delete(cross_pop, index_to_remove, axis=0)
+
+	## Uniformly cross pairs of individuals.
+	for i in range(0, cross_pop.shape[0]-1, 2):
+		for j in range(0, VOC_SIZE):
+			## Determine if a cross is going to happen.
+			rand = np.random.uniform()
+			if (rand<=0.5):
+				## Swap the two genes.
+				temp = cross_pop[i][j]
+				cross_pop[i][j] = cross_pop[i+1][j]
+				cross_pop[i+1][j] = temp
+
+	## Replace the selected individuals with their offspring.
+	np.put(pop, cross_indices, cross_pop, mode='raise')
+
+	return pop
+	
+def mutate(pop, mut_prob):
+	## Calculate a random probability for each gene.
+	r = np.random.random(size=pop.shape)	
+	## Replace every gene with a smaller probability than mut_prob with 1 or 0.
+	m = r < mut_prob
+	pop[m] = np.random.randint(0, 2, size=np.count_nonzero(m))
+
+	return pop
